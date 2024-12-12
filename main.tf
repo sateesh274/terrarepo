8ac70d9ef6f0be941793c4bedf4fa4db1b67b59e
SATEESH <119327085+sateesh274@users.noreply.github.com>
2024-12-12 11:08:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4QUKASHDSEKLAZKU"
-  secret_key = "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"
+  access_key = "AKIAQXPZC5TWJVKSLCGH"
+  secret_key = "11g7KIhYAP9UJh3kpI25ZOjlA+ifpCXs2lds8WAI"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0e6329e222e662a52"
+  ami           = "ami-0614680123427b75e"
   instance_type = var.instance_type
   tags= {
-    Name = "raham"
+    Name = "sateesh"
   }
 }
 
